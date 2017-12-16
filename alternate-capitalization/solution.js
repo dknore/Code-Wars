a360f23b96dc29d872c683e411a0d108ccb5cce8
dknore <darcy.knore@gmail.com>
2017-12-16 10:39:43 -0800

    solved kata

--- alternate-capitalization/solution.js
+++ alternate-capitalization/solution.js
@@ -1,11 +1,14 @@
 function capitalize(s) {
-  var newString = [];
+  var evenString = [];
+  var oddString = [];
   for(var i = 0; i < s.length; i++) {
     if (i % 2 !== 0) {
-      newString[i] = s[i].toUpperCase();
+      evenString[i] = s[i].toUpperCase();
+      oddString[i] = s[i].toLowerCase();
     } else {
-      newString[i] = s[i].toLowerCase();
+      evenString[i] = s[i].toLowerCase();
+      oddString[i] = s[i].toUpperCase();
     }
   }
-  return newString.join('');
+  return [oddString.join(''), evenString.join('')];
 }
