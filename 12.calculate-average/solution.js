5c13f335e4b552dd422a3a78e15759b3b6d6dc23
dknore <darcy.knore@gmail.com>
2018-01-04 20:37:45 -0800

    solved kata

--- 12.calculate-average/solution.js
+++ 12.calculate-average/solution.js
@@ -1,6 +1,5 @@
 function find_average(array) {
-    let add = array.reduce((acc, cur) => acc + cur);
-    let result = add/array.length;
-    return result;
-  }
-  
+  let add = array.reduce((acc, cur) => acc + cur);
+  let result = add / array.length;
+  return result;
+}
