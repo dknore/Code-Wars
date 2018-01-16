123e7e6fa3d9795db1ac61d9134ff329b2109ad0
dknore <darcy.knore@gmail.com>
2018-01-16 13:37:12 -0800

    solved kata

--- 08.format-words-into-sentance/solution.js
+++ 08.format-words-into-sentance/solution.js
@@ -1,13 +1,6 @@
-function removeComma(word){
-  var array = ['ninja', 'samurai', 'ronin'];
-  var string = array.toString();
-
-  for(var i = 0; i < string.length; i++) {
-    if (i = string.length - 1) {
-      return string.replace(/,/i, ' and ');
-    } else {
-      return string.join(', ');
-    }
-  }
+function formatWords (words) {
+  if (!words) return '';
+  var newArray = words.filter(arr => arr);
+  var string = newArray.join(", ").replace(/,([^,]*)$/, ' and$1');
+  return string;
 }
-removeComma();
